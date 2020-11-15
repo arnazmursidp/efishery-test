7260b61c3ffd350c11461ed1842bc338130c8e9c
Arnaz <arnazmursid@gmail.com>
2020-11-15 17:31:36 +0700

    feat(persisted-state) : Persisted State
    Add local storage lib for persisting state
    
    Closes Issues #8

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,18 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import reducer from '../pages/Reducers'
 import thunk from 'redux-thunk'
+import { loadState, saveState } from '../libs/localStorage'
 
+const persistedState = loadState()
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducer, composeEnhancers(
-    applyMiddleware(thunk)
-))
+const store = createStore(reducer, persistedState,
+    composeEnhancers(
+        applyMiddleware(thunk)
+    )
+)
+
+store.subscribe(() => {
+    saveState(store.getState())
+})
 
 export default store
